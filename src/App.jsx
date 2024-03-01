c4e9d419cfeb751aac35158d7b8e7c3f50581bdc
GeremiasAmarillo <geamarillo30@gmail.com>
2024-02-29 21:06:34 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Input } from "./components/Input";
+
+function App() {
+  return (
+    <>
+      <h1>Hello world </h1>
+      <input />
+      <input />
+      <input />
+      <input />
+    </>
+  );
+}
+
+export default App;
